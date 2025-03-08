0640581993291dce341bc94b681868660e7efbe9
hnxbc <34181415+hnxbc@users.noreply.github.com>
2025-03-08 16:51:44 +0800

    Create triangle.py

---
+++ examples/python3/triangle.py
@@ -0,0 +1,15 @@
+def generate_pascals_triangle(n):
+triangle=[[1]*(i+1)for i in range(n)]
+for i in range(2,n):
+for jin range(1，i):
+triangle[il[j]=triangle[i-1[j-1]+triangle[i -1][j]
+return triangle
+def print_pascals _triangle(triangle):
+max_width =len("".join(map(str,triangle[-1])))
+for row in triangle:
+row_str=”".join(map(str,row))
+print(row_str.center(max_width))
+if __name__=="__main__":
+n= int(input("Enter the number of rows for Pascal's Triangle: "))
+triangle =generate pascals triangle(n)
+print_pascals_triangle(triangle)
